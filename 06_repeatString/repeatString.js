dbcb76dec89db4dc3c32bc0ea1a72f3e5ac42e5c
Techron254 <leonardronald04@gmail.com>
2025-06-18 09:22:51 +0300

    solutions first batch'

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let string;
+let num;
+let i;
+const repeatString = function(string, num) {
+    let str ="";
+    if (num < 0){
+        return "ERROR";
+    }
+    else{
+        for (i = 0; i < num; i++) {
+    
+        str += string;
+    }
+  }
+return str;
 
-};
-
+    };
 // Do not edit below this line
 module.exports = repeatString;
