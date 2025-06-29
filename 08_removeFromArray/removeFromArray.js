ea7e4f3d46aa3996ff88c4ce710fa831fadb377d
Techron254 <leonardronald04@gmail.com>
2025-06-29 19:25:47 +0300

    solution for 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,7 +1,19 @@
-const removeFromArray = function(array, val) {
-    let newArray = array.filter(item => item !== val);
-    return newArray;
-};
+const Numbers = [1, 2, 3, 4];
 
+const removeFromArray = function(array, ...N) {
+    for (let i=0; i< array.length; i++){
+           for (let j=0; j < N.length; j++){
+               N.sort((a,b) => a-b);
+            if (array[i] === N[j]){
+                array.splice(i, 1);
+            }
+            if (array[i+1] === N[j]){
+                array.splice(i+1, 1)
+            }
+
+            }
+        }
+return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
