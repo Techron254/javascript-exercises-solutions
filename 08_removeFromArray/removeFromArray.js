75ff0efa85f08a183c7da147f2528710887a4b45
Techron254 <leonardronald04@gmail.com>
2025-06-29 19:30:58 +0300

    solution for 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -4,16 +4,15 @@ const removeFromArray = function(array, ...N) {
     for (let i=0; i< array.length; i++){
            for (let j=0; j < N.length; j++){
                N.sort((a,b) => a-b);
-            if (array[i] === N[j]){
+            while (array[i] === N[j]){
                 array.splice(i, 1);
             }
-            if (array[i+1] === N[j]){
-                array.splice(i+1, 1)
-            }
+
 
             }
         }
 return array;
+
 }
 // Do not edit below this line
 module.exports = removeFromArray;
