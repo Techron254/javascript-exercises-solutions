b8ebf3358447cc02d098a8c3b6af86d357a66454
Techron254 <leonardronald04@gmail.com>
2025-07-02 10:26:06 +0300

    solution for 11-temp converdion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  result = Math.round(((F-32) * 5/9)*10)/10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  result = Math.round(((C * 9/5) + 32)*10)/10;
+  return result;
 };
 
 // Do not edit below this line
