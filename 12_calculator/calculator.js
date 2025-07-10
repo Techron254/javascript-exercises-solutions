adc2330e508b8f791b1b406b22a934a2d12e8115
Techron254 <leonardronald04@gmail.com>
2025-07-10 16:54:42 +0300

    12-Calculator solution

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((a,b)=> a+b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+    return num.reduce((a,b) => a* b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let result = 1;
+  for (i = 1; i <= num ; i++){
+
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
