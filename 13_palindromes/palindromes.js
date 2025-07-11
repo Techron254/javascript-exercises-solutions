ea3728a3ab72ce463d983ae11f34ffdedefaea68
Techron254 <leonardronald04@gmail.com>
2025-07-11 13:57:58 +0300

    13-Pallindromes solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const noPunct = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?'"]/g, "");
+    joinedNoPunct= noPunct.split(' ').join('');
 
+    let reversed = joinedNoPunct.split('').reverse().join('');
+
+    if (joinedNoPunct.toLowerCase() === reversed.toLowerCase()){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
