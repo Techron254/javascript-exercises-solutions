0bcc6bf41a14030d3e08036e8e0d1aad25213175
Techron254 <leonardronald04@gmail.com>
2025-07-14 15:32:12 +0300

    14-fibonacci solutions

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
 
+const fibonacci = function(num) {
+    if (typeof(num) === String){
+        Number(num);
+    }
+    let result = [];
+    let a = 0;
+    let b = 1;
+    let next;
+    if(num < 0){
+            return ("OOPS");
+        }
+        else{
+            for (let i = 1; i<=num; i++){
+            next = a+b;
+            a=b;
+            b=next;
+            result.push(a);
+            }
+        }
+
+    if (num == 0){
+        return 0;
+    }
+    else{
+    return result[num-1];
+    }
 };
 
 // Do not edit below this line
